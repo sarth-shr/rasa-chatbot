b020dcb7282d381e3d7bed1b519359354c98bc3d
sarth-shr <126249666+sarth-shr@users.noreply.github.com>
2023-03-05 17:49:40 +0545

    Implement ActionRememberName class

--- actions/actions.py
+++ actions/actions.py
@@ -13,22 +13,29 @@ balance_db = {
     "Jill": "3000",
 }
 
+class ActionRememberName(Action):
+    def name(self) -> Text:
+        return "action_remember_name"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, 
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        current_name = next(tracker.get_latest_entity_values("account_name"), None)
+
+        return [SlotSet("account_name", current_name)]
+
 
 class ActionReturnBalance(Action):
     def name(self) -> Text:
         return "action_return_balance"
 
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict[Text, Any]]:
-        current_name = next(
-            tracker.get_latest_entity_values("account_name"), None)
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        current_name = tracker.get_slot("account_name")
+        current_number = next(tracker.get_latest_entity_values("account_number"), None)
+
 
         balance_amt = balance_db.get(current_name, None)
-        msg = f"Dear {current_name}, your account has a balance of Rs. {balance_amt}."
+        msg = f"Dear {current_name}, your account number: {current_number} has a balance of Rs. {balance_amt}."
         dispatcher.utter_message(text=msg)
 
         return []
